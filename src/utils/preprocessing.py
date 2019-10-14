09f2fc55c2da60ffae885753f63387694f93a62e
Thierry <thierry.bossy@romandie.com>
2019-10-14 20:23:02 +0200

    preprocessing

---
+++ src/utils/preprocessing.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def most_frequent(x, extended_output=False):
+    """
+    Get the most frequent value in an array
+    """
+    counter = {}
+    max_val = x[0]
+    counter[max_val] = 1
+    for val in x[1:]:
+        if(val in counter):
+            counter[val] += 1
+            if counter[val] > counter[max_val]:
+                max_val = val
+        else:
+            counter[val] = 1
+                
+    return (max_val, counter[max_val]) if extended_output else max_val
+
+def replace(x, err_val, find_replacement):
+    """
+    Replace each occurence of a specified value in an array
+    according to a specified replacement function
+    """
+    replacement = find_replacement(x[x != err_val])
+
+    replaced = x.copy()
+    replaced[replaced == err_val] = replacement
+    
+    return replaced
+   
+def clean_data(x, err_val, find_replacement):
+    """
+    Clean a matrix by replacing errors values in each column
+    according to a specified replacement function
+    """
+    x_clean = np.zeros(x.shape)
+    nb_features = x.shape[1]
+    
+    for feature in range(nb_features):
+        x_clean[:, feature] = replace(x[:, feature], err_val, find_replacement)
+        
+    return x_clean
