0711b82ee55b14598788eb9358e702fc35d59316
raphstrebel <raphael.strebel@epfl.ch>
2019-10-23 10:29:05 +0200

    simplified methods to keep minimum in test, also split y

--- src/utils/jet_num_handler.py
+++ src/utils/jet_num_handler.py
@@ -8,6 +8,7 @@ def find_jet_num_column(x):
             return col_idx
         
 def get_undef_cols_idx(m, undef_val=-999):
+    """Get the indices of columns that have only undefined values"""
     undef_col_idx = []
     for col_idx in range(m.shape[1]):
         column = m[:,col_idx]
@@ -16,5 +17,15 @@ def get_undef_cols_idx(m, undef_val=-999):
     return undef_col_idx
 
 def get_x_jet_num_reduced(x_jet_num, undef_val=-999):
+    """Delete undefined columns of x"""
     undef_col_idx = get_undef_cols_idx(x_jet_num)
     return np.delete(x_jet_num, undef_col_idx, 1)
+
+def get_splitted_reduced_x_y_from_jet_num_val(x, y, jet_num_idx, jet_num_val):
+    """Split and reduce (reduce only x) x and y according to the jet_num values"""
+    # Split data into subset according to jet_num value
+    jet_num_val_indices = np.argwhere(x[:, jet_num_idx] == jet_num_val).flatten()
+    x_jet_num_val, y_jet_num_val = x[jet_num_val_indices], y[jet_num_val_indices]
+    # Remove unnecessary columns of x
+    x_jet_num_val_reduced = get_x_jet_num_reduced(x_jet_num_val)
+    return x_jet_num_val_reduced, y_jet_num_val
