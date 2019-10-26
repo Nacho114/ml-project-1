c0eb16762ef3a79cd63d9ef3676dd8fafb578d35
raphstrebel <raphael.strebel@epfl.ch>
2019-10-26 11:56:45 +0200

    concat jetnum = 2 and 3

--- src/utils/jet_num_handler.py
+++ src/utils/jet_num_handler.py
@@ -20,8 +20,24 @@ def split_by_jet_num(x, y):
         x_split.append(x[mask])
         if not y is None:
             y_split.append(y[mask])
+            
+    x_split_23 = np.concatenate((x_split[2], x_split[3]), axis=0)
+            
+    x_split_reduce = []
+    x_split_reduce.append(x_split[0])
+    x_split_reduce.append(x_split[1])
+    x_split_reduce.append(x_split_23)
+    
+    y_split_reduce = []
+    
+    if not y is None:
+        y_split_23 = np.concatenate((y_split[2], y_split[3]), axis=0)
+        
+        y_split_reduce.append(y_split[0])
+        y_split_reduce.append(y_split[1])
+        y_split_reduce.append(y_split_23)
 
-    return x_split, y_split
+    return x_split_reduce, y_split_reduce
 
 def clean_split(x_split):
     clean_x_split = []
@@ -29,7 +45,8 @@ def clean_split(x_split):
     undef_idx_of_jet_num = constant.UNDEF_IDX_OF_JET_NUM
 
     for idx, x in enumerate(x_split):
-        undef_col_idx = undef_idx_of_jet_num[idx]
+        undef_col_idx = undef_idx_of_jet_num[idx].copy()
+        undef_col_idx.append(constant.JET_NUM_COL)
         new_x = np.delete(x, undef_col_idx, 1)
         clean_x_split.append(new_x)
 
