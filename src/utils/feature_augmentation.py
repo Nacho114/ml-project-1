ce9cbd00542d5e22d33beb043b3bb9e7211ffff5
Thierry <thierry.bossy@romandie.com>
2019-10-26 16:46:17 +0200

    build poly optimized

--- src/utils/feature_augmentation.py
+++ src/utils/feature_augmentation.py
@@ -2,8 +2,12 @@ import numpy as np
 
 
 def build_poly(x, degree):
-    """polynomial basis functions for input data x, for j=1 up to j=degree."""
-    x_exp = x
-    for j in range(2, degree+1):
-        x_exp = np.concatenate((x_exp, x**j), axis=1)
-    return x_exp
+    n = len(x)
+    vector = np.ones((1, n))
+    matrix = vector
+    
+    for column in range(degree):
+        vector = vector * x
+        matrix = np.concatenate((matrix, vector), axis=0)
+    
+    return matrix.T
