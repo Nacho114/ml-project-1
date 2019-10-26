62fcc24becd019c241555a6dab4e17882ef30bb7
Thierry <thierry.bossy@romandie.com>
2019-10-26 17:47:32 +0200

    build poly take array

--- src/utils/feature_augmentation.py
+++ src/utils/feature_augmentation.py
@@ -1,19 +1,13 @@
 import numpy as np
     
-def build_poly(x, degree):
-    """polynomial basis functions for input data x, for j=2 up to j=degree."""
-    if(degree < 2):
-        return np.array([])
+def build_poly(x, degrees):
+    """polynomial basis functions for input data x, for j=1 up to j=degree."""
 
-    n = len(x)
-    vector = x*x
-    matrix = vector
-    
-    for column in range(2, degree):
-        vector = vector * x
-        matrix = np.concatenate((matrix, vector), axis=0)
-    
-    return matrix.T
+    x_exp = x**degrees[0]
+    for degree in degrees[1:]:
+        x_exp = np.concatenate((x_exp, x**degree), axis=0)
+
+    return x_exp
     
 def add_bias(x)
     return = np.ones((x.shape[0], 1))
