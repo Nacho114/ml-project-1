d5ac7897da2657e22e7400ca2fac2743585ce3af
Thierry <thierry.bossy@romandie.com>
2019-10-26 16:53:07 +0200

    build poly optimized

--- src/utils/feature_augmentation.py
+++ src/utils/feature_augmentation.py
@@ -2,11 +2,14 @@ import numpy as np
 
 
 def build_poly(x, degree):
+    if(degree < 2):
+        return np.array([])
+
     n = len(x)
-    vector = np.ones((1, n))
+    vector = x*x
     matrix = vector
     
-    for column in range(degree):
+    for column in range(2, degree):
         vector = vector * x
         matrix = np.concatenate((matrix, vector), axis=0)
     
